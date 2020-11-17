a3747d2c060d37438d783a97542178809ea444da
fakhryfernanda <mff023@gmail.com>
2020-11-17 21:17:08 +0700

    third commit

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 // Color Flipper
 // =============
 
-var hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 
-var btn = document.querySelector(".btn");
-var color = document.querySelector(".color");
+const colorFrame = document.getElementById("color-flipper")
+const btn = document.querySelector(".btn");
+const color = document.querySelector(".color");
 
 btn.addEventListener("click", function() {
     var hexColor = "#";
@@ -15,5 +16,45 @@ btn.addEventListener("click", function() {
     }
 
     color.textContent = hexColor;
-    document.body.style.backgroundColor = hexColor;    
+    colorFrame.style.backgroundColor = hexColor;    
+});
+
+// ========
+// COUNTER
+// ========
+
+var count = 0;
+const numberCounted = document.getElementById("number-counted");
+const decreaseButton = document.getElementById("decrease-btn");
+const resetButton = document.getElementById("reset-btn");
+const increaseButton = document.getElementById("increase-btn");
+
+decreaseButton.addEventListener("click", function() {
+    count--;
+    numberCounted.textContent = count;
+    if (count<0) {
+        numberCounted.style.color = "red";
+    } else if (count==0){
+        numberCounted.style.color = "black"
+    } else {
+        numberCounted.style.color = "green"
+    }
+});
+
+resetButton.addEventListener("click", function(){
+    count = 0;
+    numberCounted.textContent = 0;
+    numberCounted.style.color = "black";
+});
+
+increaseButton.addEventListener("click", function() {
+    count++;
+    numberCounted.textContent = count;
+    if (count<0) {
+        numberCounted.style.color = "red";
+    } else if (count==0){
+        numberCounted.style.color = "black"
+    } else {
+        numberCounted.style.color = "green"
+    }
 });
